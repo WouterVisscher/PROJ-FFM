5daf8d27b2c1b3ae3a723895a8c6ecf07f55ad0d
Wouter <wouter.visscher@gmail.nl>
2025-11-24 14:37:10 +0100

    update coords

--- src/main/java/nl/kadaster/App.java
+++ src/main/java/nl/kadaster/App.java
@@ -10,10 +10,10 @@ public class App
     public static void main( String[] args )
     {
         Transformer transformer = new Transformer("EPSG:28992", "EPSG:9067");
-        Coordinate coord1 = transformer.transformLPZ( 155000, 463000, 0);
-        Coordinate coord2 = transformer.transformLPZ( 155000, 463000, 0);
+        Coordinate coord1 = transformer.transformLPZ( 155029, 463001, 0);   // Amersfoort O.L. Vrouwetoren Amersfoort
+        Coordinate coord2 = transformer.transformLPZ( 	194632, 469365, 0);  // Apeldoorn AGRS de Grift
 
-        Coordinate coord3 = transformer.transformLPZ( 155000, 463000, 0);
+        Coordinate coord3 = transformer.transformLPZ( 194164, 465895, 0); // Amersfoort NETPOS de Brug
 
 
         System.out.println("Transformed Coordinate: " + coord1);
